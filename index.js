765955b2f2efc2b295b60a8edfd97466b0638aec
Von C <shervonc@gmail.com>
2019-07-15 19:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
+function happyHolidayto(holiday,name) {
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+}
